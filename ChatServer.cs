aecaea597e787866bdce1cacf042e792ae649cf1
Danila <danchik.b@mail.ru>
2024-02-29 01:02:07 +0300

    HW3

---
+++ ChatServer.cs
@@ -0,0 +1,117 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace Server
+{
+  public class ChatServer:IDisposable
+  {
+    TcpListener? _listener;
+    static List<TcpClient> clients = [];
+    static CancellationToken? ct2;
+
+    public ChatServer(IPEndPoint endPoint)
+    {
+      if (endPoint != null)
+        _listener = new TcpListener(endPoint);
+    }
+
+    public async Task Run(CancellationToken ct)
+    {
+      try
+      {
+        _listener?.Start();
+        Console.WriteLine("Сервер запущен");
+
+        while (true)
+        {
+          TcpClient? tcpClient = await _listener?.AcceptTcpClientAsync();
+          Console.WriteLine($"{tcpClient?.GetHashCode()} подключен!");
+
+          Task.Run(() => ProcessClient(tcpClient), ct);
+
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+
+    public static async Task ProcessClient(TcpClient? tcpClient)
+    {
+      try
+      {
+        if (!clients.Contains(tcpClient))
+        {
+          clients.Add(tcpClient);
+
+          foreach (var client in clients)
+          {
+            if (client.GetHashCode() != tcpClient.GetHashCode())
+            {
+              StreamWriter writer = new(client.GetStream());
+
+              await writer.WriteLineAsync(tcpClient.GetHashCode() + " присоединился");
+              writer.Flush();
+            }
+          }
+        }
+
+        StreamReader reader = new(tcpClient.GetStream());
+
+        while (true)
+        {
+          var message = reader.ReadLine();
+
+          if (!string.IsNullOrEmpty(message))
+          {
+            foreach (var client in clients)
+            {
+              StreamWriter writer = new(client.GetStream());
+
+              if (client.GetHashCode() != tcpClient.GetHashCode())
+                await writer.WriteLineAsync(tcpClient.GetHashCode() + ": " + message);
+              else
+              {
+                await writer.WriteLineAsync("Сообщение доставлено!");
+              }
+              writer.Flush();
+            }
+
+            Console.WriteLine(tcpClient.GetHashCode() + ": " + message);
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+      finally
+      {
+        DisposeClient(tcpClient);
+      }
+    }
+
+    public static bool DisposeClient(TcpClient tcpClient)
+    {
+      if (clients.Contains(tcpClient))
+      {
+        tcpClient.Dispose();
+        return clients.Remove(tcpClient);
+      }
+
+      return false;
+    }
+
+    public void Dispose()
+    {
+      _listener?.Dispose();
+      _listener = null;
+
+      foreach (var client in clients)
+        client.Dispose();
+
+      clients.Clear();
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,103 +1,36 @@
-﻿using System.Net;
-using System.Net.Sockets;
+﻿using Server;
+using System.Net;
 
 namespace Seminar1;
 
 internal class Program
 {
-  static void Main(string[] args)
+  static void Main()
   {
-    var cs = new ChatServer();
+    CancellationTokenSource cts = new CancellationTokenSource();
+    CancellationToken ct = cts.Token;
 
-    cs.Run();
+    IPEndPoint iPEndPoint = new(IPAddress.Any, 55555);
+    using ChatServer cs = new(iPEndPoint);
+
+    var task = cs.Run(ct);
+
+    string? serverMessage = string.Empty;
+    while (!serverMessage.Equals("exit", StringComparison.InvariantCultureIgnoreCase) &&
+           !serverMessage.Equals("quit", StringComparison.InvariantCultureIgnoreCase))
+    {
+      serverMessage = Console.ReadLine();
+    }
+
+    try
+    {
+      cts.Cancel();
+    }
+    finally
+    {
+      cs.Dispose();
+    }
 
     Console.ReadKey();
-    Environment.Exit(0);
-  }
-}
-
-public class ChatServer
-{
-  TcpListener listener = new TcpListener(IPAddress.Any, 55555);
-  static List<TcpClient> clients = new List<TcpClient>();
-
-  public async Task Run()
-  {
-    try
-    {
-      listener.Start();
-      await Console.Out.WriteLineAsync("Сервер запущен");
-
-      while (true)
-      {
-        var tcpClient = await listener.AcceptTcpClientAsync();
-        await Console.Out.WriteLineAsync($"{tcpClient.GetHashCode()} подключен!");
-
-        _ = Task.Run(() => ProcessClient(tcpClient));
-
-        _ = Task.Run(() => SendMessageFromClient(tcpClient));
-      }
-    }
-    catch (Exception ex)
-    {
-      Console.WriteLine(ex.Message);
-    }
-  }
-
-  public static async Task ProcessClient(TcpClient tcpClient)
-  {
-
-    if (!clients.Contains(tcpClient))
-    {
-      clients.Add(tcpClient);
-
-      foreach (var client in clients)
-      {
-        if (client.Connected && client.GetHashCode() != tcpClient.GetHashCode())
-        {
-          StreamWriter writer = new StreamWriter(client.GetStream());
-
-          await writer.WriteLineAsync(tcpClient.GetHashCode() + " присоединился");
-          writer.Flush();
-        }
-      }
-    }
-  }
-
-  public static async Task SendMessageFromClient(TcpClient tcpClient)
-  {
-
-    try
-    {
-      var reader = new StreamReader(tcpClient.GetStream());
-      string? message;
-    
-      while (tcpClient.Connected)
-      {
-        if (!string.IsNullOrEmpty(message = reader.ReadLine()))
-        {
-          foreach (var client in clients)
-          {
-            StreamWriter writer = new StreamWriter(client.GetStream());
-            
-            if (client.Connected && client.GetHashCode() != tcpClient.GetHashCode())
-            {
-              await writer.WriteLineAsync(tcpClient.GetHashCode() + ": " + message);
-            }
-            else if (client.Connected)
-            {
-              await writer.WriteLineAsync("Сообщение доставлено!");
-            }
-            writer.Flush();
-          }
-
-          Console.WriteLine(tcpClient.GetHashCode() + ": " + message);
-        }
-      }
-    }
-    catch (Exception ex)
-    {
-      Console.WriteLine(ex.Message);
-    }
   }
 }
